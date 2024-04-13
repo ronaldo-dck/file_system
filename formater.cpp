4792a47b54b333cdce05b4e95e434b6291092caf
Ronaldo Drecksler <estudansator@gmail.com>
2024-04-12 22:16:18 -0300

    formater pronto, refatorar

--- formater.cpp
+++ formater.cpp
@@ -13,11 +13,22 @@ typedef struct bootRecord
     char SectorsPerCluster;
     unsigned short int ReservedSectors;
     unsigned long int TotalOfSectors;
-    unsigned long int SizeOfBitMAp;
-    unsigned long int INODES;
+    unsigned long int SizeOfBitMap;
+    unsigned long int Inodes;
     unsigned short int LenOfInode;
     unsigned long int PosData;
 } __attribute__((packed)) bootRecord;
+
+typedef struct Inode 
+{
+    char Type;
+    char Alignment[3];
+    unsigned long int Size;
+    unsigned long int ModificationTime;
+    unsigned long int CreationTime;
+    unsigned int Clusters[8];
+    unsigned int IndirectCluster;
+} __attribute__((packed)) Inode;
 
 bootRecord define_br(unsigned long int size_of_img, unsigned int sector_per_cluster)
 {
@@ -28,47 +39,84 @@ bootRecord define_br(unsigned long int size_of_img, unsigned int sector_per_clus
     br.TotalOfSectors = size_of_img;
     br.ReservedSectors = 1;
 
-
     size_of_img--;
 
-    /* 
-        4096 total de bit em um setor de 512 bytes
+    float TotalAgrupamentos = (br.TotalOfSectors / (br.LenOfInode * 8 + (br.BytesPerSector * 8 * br.SectorsPerCluster) + 1));
+    br.SizeOfBitMap = int(TotalAgrupamentos);
+    br.Inodes = int(TotalAgrupamentos) * BYTES_PER_SECTOR * 8;
 
-        4096 * len(inode) / 512
+    int v_exedente = size_of_img - int(TotalAgrupamentos) * (br.LenOfInode * 8 + (br.BytesPerSector * 8 * br.SectorsPerCluster) + 1);
 
-
-    
-    
-     */
-
-    float total_agrupamentos = (br.TotalOfSectors / ( br.LenOfInode * 8 + (br.BytesPerSector * 8 * br.SectorsPerCluster) + 1));
-    br.SizeOfBitMAp = int(total_agrupamentos);
-    br.INODES = int(total_agrupamentos) * BYTES_PER_SECTOR * 8 ;
-
-
-
-    int v_execente = size_of_img - int(total_agrupamentos) * (br.LenOfInode * 8 + (br.BytesPerSector * 8 * br.SectorsPerCluster) + 1);
-
-    if (v_execente >= (1 + 1 + 8 * sector_per_cluster))
+    if (v_exedente >= (1 + 1 + 8 * br.SectorsPerCluster))
     {
-        br.SizeOfBitMAp++;
-        br.INODES += BYTES_PER_SECTOR / br.LenOfInode;
-        v_execente -= 2;
-        if (v_execente % sector_per_cluster)
-            br.ReservedSectors += v_execente % sector_per_cluster;
+        br.SizeOfBitMap++;
+        br.Inodes += BYTES_PER_SECTOR / br.LenOfInode;
+        v_exedente -= 2;
+        if (v_exedente % br.SectorsPerCluster)
+            br.ReservedSectors += v_exedente % br.SectorsPerCluster;
     }
     else
-        br.ReservedSectors += v_execente;
+        br.ReservedSectors += v_exedente;
 
-    br.PosData = br.ReservedSectors + br.SizeOfBitMAp + (br.INODES/(br.BytesPerSector/br.LenOfInode));
+    br.PosData = br.ReservedSectors + br.SizeOfBitMap + (br.Inodes / (br.BytesPerSector / br.LenOfInode));
+
 
     return br;
 }
 
+void clean_ghost_clusters(const string &filename, const bootRecord &BR)
+{
+    fstream file(filename, ios::binary | ios::out | ios::in);
 
+    int disponiveis = BR.TotalOfSectors - BR.PosData;
+    cout << disponiveis << endl;
 
+    file.seekp(BR.ReservedSectors * BR.BytesPerSector + disponiveis / 8 - 1, ios::beg);
 
+    uint8_t byte = 0xFF;
+    short resto = disponiveis % 8;
+    byte >>= resto;
 
+    file.write(reinterpret_cast<char *>(&byte), sizeof(uint8_t));
+
+    byte = -1;
+    for (streampos p = file.tellp(); p < (BR.ReservedSectors + BR.SizeOfBitMap) * BR.BytesPerSector; p+=1)
+    {
+        file.write(reinterpret_cast<char *>(&byte), sizeof(byte));
+    }
+}
+
+void create_root_dir(const string &filename, const bootRecord &BR)
+{
+    fstream file(filename, ios::binary | ios::out | ios::in);
+
+    Inode inode;
+    //char Type;
+    inode.Type = 0x01;
+    //char Alignment[3];
+    //unsigned long int Size;
+    inode.Size = 0;
+    //unsigned int ModificationTime;
+    time_t now = time(0);
+    inode.ModificationTime = static_cast<unsigned int>(now);
+    //unsigned int CreationTime;
+    inode.CreationTime = static_cast<unsigned int>(now);
+    //unsigned int Clusters[8];
+    inode.Clusters[0] = 1;
+    for (int i = 1; i < 8; i++)
+    {
+        inode.Clusters[i] = 0;
+    }
+    // unsigned int IndirectCluster;
+    inode.IndirectCluster = 0;
+
+    file.seekp((BR.ReservedSectors) * BR.BytesPerSector, ios::beg);
+    int8_t byte = 0x80;
+    file.write(reinterpret_cast<const char *>(&byte), sizeof(byte));
+    file.seekp(0, ios::cur);
+    file.seekp((BR.ReservedSectors + BR.SizeOfBitMap) * BR.BytesPerSector, ios::beg);
+    file.write(reinterpret_cast<const char *>(&inode), sizeof(Inode));
+}
 
 void write_br_to_file(const string &filename, const bootRecord &BR)
 {
@@ -88,9 +136,6 @@ void write_br_to_file(const string &filename, const bootRecord &BR)
     // Fechar o arquivo
     file.close();
 }
-
-
-
 
 bootRecord read_br_from_file(const string &filename)
 {
@@ -119,17 +164,18 @@ void print_br(const bootRecord &BR)
     cout << "SectorsPerCluster: " << static_cast<int>(BR.SectorsPerCluster) << endl;
     cout << "ReservedSectors: " << BR.ReservedSectors << endl;
     cout << "TotalOfSectors: " << BR.TotalOfSectors << endl;
-    cout << "SizeOfBitMAp: " << BR.SizeOfBitMAp << endl;
-    cout << "INODES: " << BR.INODES << endl;
+    cout << "SizeOfBitMap: " << BR.SizeOfBitMap << endl;
+    cout << "Inodes: " << BR.Inodes << endl;
     cout << "LenOfInode: " << BR.LenOfInode << endl;
+    cout << "PosData: " << BR.PosData << endl;
 }
 
 int main()
 {
-    /* 
+    /*
     TO - DO:
-        -- bitmap clusters fantasmas 
-        -- criar o Inode do root dir
+        -- bitmap clusters fantasmas ✅
+        -- criar o Inode do root dir ✅
      */
 
     long int size_of_img = 546;
@@ -159,6 +205,8 @@ int main()
 
     // Escrever o bootRecord no arquivo
     write_br_to_file(name_img, BR);
+    clean_ghost_clusters(name_img, BR);
+    create_root_dir(name_img, BR);
 
     // Ler o bootRecord do arquivo
     bootRecord read_BR = read_br_from_file(name_img);
